c663ee5695aa6a0bd119fe6dda363ae273af1253
Binod <Binod@DESKTOP-EU7BKOJ>
2023-09-12 15:56:54 +0530

    Commit And Push

--- src/test/java/demo/Sample.java
+++ src/test/java/demo/Sample.java
@@ -4,6 +4,6 @@ public class Sample {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hi this is my first commit");
+		System.out.println("Hi this is my first commit.");
 	}
 }
